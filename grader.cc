a9dbd6c9f93d19aef2a525fb7989c59be231c217
Ouoluj <fabaker@gmail.com>
2018-11-07 11:43:31 -0500

    Added mostly blank homework 7.

---
+++ grader.cc
@@ -0,0 +1,26 @@
+//****************************************************************
+//
+//  Program:        Test Grader
+//
+//  Name:           Derek Baker
+//  Email:          db820017@ohio.edu
+//
+//  Section:        100 (Nasseef Abukamail)
+//
+//  Description:    .
+//
+//  Date:           11-13-18
+//
+//****************************************************************
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <fstream>
+#include <cstring>
+using namespace std;
+
+int main(int argc, char **argv) {
+
+  return 0;
+}
