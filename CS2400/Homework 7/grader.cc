6a610f4274ebb5c712658a1f9433f3d4cc62f2b7
Ouoluj <db820017@ohio.edu>
2018-11-07 22:24:19 -0500

    Doot.

--- CS2400/Homework 7/grader.cc
+++ CS2400/Homework 7/grader.cc
@@ -26,43 +26,42 @@ int main(int argc, const char **argv) {
     cout << "No valid input file." << endl;
     return 1;
   }
-  else {
-    ifstream inputFile;
-    inputFile.open(argv[1]);
-    if (inputFile.fail()) { // Returns 1 if the input file cannot be opened.
-      return 1;
-    }
+
+  ifstream inputFile;
+  inputFile.open(argv[1]);
+  if (inputFile.fail()) { // Returns 1 if the input file cannot be opened.
+
   }
-  if (argc == 3) { // Opens the output file if one is given. If not, prints to the console.
-    ofstream outputFile;
-    outputFile.open(argv[2]);
-  }
+  // if (argc == 3) { // Opens the output file if one is given. If not, prints to the console.
+  //   ofstream outputFile;
+  //   outputFile.open(argv[2]);
+  // }
 
   // vector <char> key = {'A', 'C', 'D', 'B', 'C', 'B', 'D', 'C', 'C', 'A', 'C', 'D', 'A', 'C', 'C'};
-  vector <char> key;
-  vector <char> answers;
+  char key[20];
+  char answers[20];
   char ch;
   string firstName, lastName;
 
   inputFile >> key;
-  for (size_t i = 0; i < key.size(); i++) {
+  for (size_t i = 0; i < 15; i++) {
     cout << key[i];
   }
-  // while (!inputFile.eof()) {
-  //   inputFile >> firstName >> lastName;
-  //
-  //   inputFile.get(ch);
-  //   while (inputFile.get(ch) != '\n') {
-  //     for (size_t i = 1; i < key.size(); i++) {
-  //
-  //     }
-  //   }
-  // }
+  while (!inputFile.eof()) {
+    inputFile >> firstName >> lastName;
+
+    inputFile.get(ch);
+    while (ch != '\n') {
+      for (size_t i = 1; i < 15; i++) {
+
+      }
+    }
+  }
 
 
   inputFile.close();
-  if (argc == 3) {
-    outputFile.close();
-  }
+  // if (argc == 3) {
+  //   outputFile.close();
+  // }
   return 0;
 }
