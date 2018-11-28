ef92c65586c1d47c0e9a36e8151723b851ce4227
Ouoluj <fabaker@gmail.com>
2018-11-28 11:48:13 -0500

    Created files for next assignment.

---
+++ CS2400/Homework 8/Book.h
@@ -0,0 +1,20 @@
+//****************************************************************
+//
+//  Program:        .
+//
+//  Name:           Derek Baker
+//  Email:          db820017@ohio.edu
+//
+//  Section:        100 (Nasseef Abukamail)
+//
+//  Description:    .
+//
+//  Date:           .
+//
+//****************************************************************
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <fstream>
+using namespace std;
