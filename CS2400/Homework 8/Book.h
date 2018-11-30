e5c5e7babcc656ccf4e719f1615480da6f54618f
Ouoluj <fabaker@gmail.com>
2018-11-29 21:21:02 -0500

    Almost finished.

--- CS2400/Homework 8/Book.h
+++ CS2400/Homework 8/Book.h
@@ -1,20 +1,36 @@
-//****************************************************************
-//
-//  Program:        .
-//
-//  Name:           Derek Baker
-//  Email:          db820017@ohio.edu
-//
-//  Section:        100 (Nasseef Abukamail)
-//
-//  Description:    .
-//
-//  Date:           .
-//
-//****************************************************************
+#ifndef BOOK
+#define BOOK
 
-#include <iostream>
-#include <iomanip>
-#include <cstdlib>
-#include <fstream>
 using namespace std;
+class Book {
+
+  private:
+  string title;
+  int year;
+  string author;
+
+  public:
+  // Constructors
+  Book();
+  Book(string newTitle, int newYear, string newAuthor);
+  Book(string allData);
+
+  // Setters
+  void setTitle(string newTitle);
+  void setYear(int newYear);
+  void setAuthor(string newAuthor);
+
+  // Getters
+  string getTitle();
+  int getYear();
+  string getAuthor();
+
+  // Helpers
+  string makeLower(string target);
+  bool matchTitle(string targetTitle);
+  bool matchAuthor(string targetAuthor);
+  bool matchYear(string targetYear);
+  bool match(string target);
+  void outputBook();
+};
+#endif
