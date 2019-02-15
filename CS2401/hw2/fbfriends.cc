4358388c1c108f224de345916919ba7641008149
ouoluj <38358448+ouoluj@users.noreply.github.com>
2019-02-14 23:01:25 -0500

    Wrote "the Big 3" functions

--- CS2401/hw2/fbfriends.cc
+++ CS2401/hw2/fbfriends.cc
@@ -6,16 +6,39 @@
 //
 //
 //****************************************************************
+#include <algorithm>
 #include "fbfriends.h"
 
-FBFriends::FBFriends()
+FBFriends::FBFriends() {
+  data = new Friend[capacity];
+  capacity = 5;
+  used = 0;
+  current_index = 0;
+}
 
-FBFriends::~FBFriends()
+// Big 3
+FBFriends::~FBFriends() {
+  delete []data;
+  std::cout << "Goodbyyyee" << '\n';
+}
 
-FBFriends::FBFriends(const FBFriends& other)
+FBFriends::FBFriends(const FBFriends& other) {
+  used = other.used;
+  capacity = other.capacity;
+  data = new Friend data[capacity];
+  copy(other.data, other.data + used, data);
+}
 
-void FBFriends::operator = (const FBFriends& other)
+void FBFriends::operator = (const FBFriends& other) {
+  if (!(this == &other)) {
+    delete []data;
+    capacity = other.capacity;
+    data = new Friend data[capacity];
+    copy(other.data, other.data + used, data);
+  }
+}
 
+// Functions for internal iterator
 void FBFriends::start()
 
 void FBFriends::advance()
@@ -40,8 +63,16 @@ bool FBFriends::is_friend(const Friend& f) const
 
 void FBFriends::load(std::istream& ins)
 
-void FBFriends::save(std::ostream& out
+void FBFriends::save(std::ostream& out)
 
 
 
-void FBFriends::resize() // increases the capacity of the container by 5
+void FBFriends::resize() {
+  Friend *tmp;
+  tmp = new Friend[capacity + 5];
+  for (size_t i = 0; i < used; i++)
+    {tmp[i] = data[i];}
+  delete []data;
+  data = tmp;
+  capacity += 5;
+} // increases the capacity of the container by 5
