5b68af12ed524f3949a9d7fbf7a2f1320bf89a89
ouoluj <38358448+ouoluj@users.noreply.github.com>
2019-03-09 01:14:50 -0500

    Made progress in most functions.

--- CS2401/hw3/carlist.cc
+++ CS2401/hw3/carlist.cc
@@ -13,8 +13,19 @@ Carlist::Carlist() {
 }
 
 Carlist::Carlist(const Carlist& other) {
-  head = other.head;
+  if(other.head == NULL) {head = NULL;}
+  else {
+    node *sptr = other.head;
+    node *dptr = head;
+    head = new node(other.head.data(), NULL);
 
+    sptr = sptr.link();
+    while (sptr != NULL) {
+      dptr.set_link(new node(sptr.data(), NULL));
+      dptr = dptr.link();
+      sptr = sptr.link();
+    }
+  }
 }
 
 // void Carlist::operator =(const Carlist& other) {
@@ -57,6 +68,80 @@ void Carlist::add(Car c) {
         previous = previous.link();
         current = current.link();
       }
+    }
+  }
+}
+
+void Carlist::showall(std::ostream& outs)const {
+  node *cursor = head;
+  while (cursor != NULL) {
+    cout << cursor << endl;
+    cursor = cursor.link();
+  }
+}
+
+void Carlist::view_all_of_year(int target)const {
+  node *cursor = head;
+  while (cursor != NULL) {
+    if (cursor.data().get_year() == target) {
+      cout << cursor << endl;
+    }
+    cursor = cursor.link();
+  }
+}
+
+void Carlist::view_all_of_make(std::string target)const {
+  node *cursor = head;
+  while (cursor != NULL) {
+    if (cursor.data().get_make() == target) {
+      cout << cursor << endl;
+    }
+    cursor = cursor.link();
+  }
+}
+
+Car Carlist::cheapest()const {
+  node *cursor = head;
+  Car cheapest = head.data();
+
+  while (cursor != NULL) {
+    if (cursor.data().get_price() < cheapest.get_price()) {
+      cheapest = cursor.data();
+    }
+    cursor = cursor.link();
+  }
+}
+
+double Carlist::tax_value()const {
+  double total = 0, num = 0, value = 0;
+  node *cursor = head;
+
+  while (cursor != NULL) {
+    total += cursor.data().get_price();
+    num ++;
+    cursor = cursor.link();
+  }
+  value = total/num;
+  return value;
+}
+
+void Carlist::remove(Car c) {
+  node *cursor = head;
+  if (head.data() == c) {
+    head = head.link();
+    delete cursor;
+  }
+
+  else {
+    node *previous = head;
+    cursor = head.link();
+    while (cursor != NULL) { // Cursor is already initialized to head's link, which, if null, will not enter loop
+      if (cursor.data() == c) {
+        previous.set_link(cursor.link());
+        delete cursor;
+      }
+      previous = previous.link();
+      cursor = cursor.link();
     }
   }
 }
